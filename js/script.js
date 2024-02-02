40504705b6bb1a3fe08deb81ea91fceec53d24c6
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2024-02-01 23:19:44 -0500

    Build function createSquare() for the unitary squares in the game's grid

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+const gridContainer = document.querySelector('.eas-container');
+
 let gridSize = 16;
 
+function createSquare() {
+    const newSquare = document.createElement('div');
+    const dimension = '20px';
+
+    newSquare.classList.add('eas-square');
+    newSquare.style.width = dimension;
+    newSquare.style.height = dimension;
+    newSquare.style.border = 'solid purple 1px';
+
+    return newSquare;
+}
+
+const newSquare = createSquare();
+const otherSquare = createSquare();
+
+gridContainer.appendChild(newSquare);
+gridContainer.appendChild(otherSquare);
