40dcc14a29f3eeb11f51e01f7e6520c9a90f5204
Cristian Estrada <59891802+cdestradab@users.noreply.github.com>
2024-02-03 01:01:20 -0500

    Add the logic for painting the grid correctly.

--- PSEUDO.md
+++ PSEUDO.md
@@ -20,11 +20,13 @@ GENERAL OBJECTIVE: Build an "etch-a-sketch" game, whatever it is, following the 
             OK-         LATER: Use a loop to create every square, give it an ID called "cX-rY" where X is the number of the column, and Y is the number of the row"
             OK- Declare a function "buildGrid" to create a series of rows using "buildRow", taking "size" as an input
             
-            - Make the unitary elements of the grid to fill the dimensions of the grid container, all of them have the same dimensions.
-            - Make the createGrid() function create grids with a specific dimension from a given input.
-            - Write all the styles made with the JS scripts in the CSS file instead.
+            OK- Make the unitary elements of the grid to fill the dimensions of the grid container, all of them have the same dimensions.
+            OK- Make the createGrid() function create grids with a specific dimension from a given input.
+            OK- Write all the styles made with the JS scripts in the CSS file instead.
 
 2. Set up a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+        OK- Create a variable "hoverElement" for a "hover" event, that captures the id of the element.
+        OK- Create a function "changeColor" that takes "hoverElement" and a HEX color code as input, and change the background color of the element.
 
 3. Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid.
    Once entered, the existing grid should be removed and a new grid should be generated in the same total space as before.
--- js/script.js
+++ js/script.js
@@ -1,31 +1,26 @@
 const gridContainer = document.querySelector('.eas-container');
 
 let gridSize = 16;
+let brushColor = 'brown';
 
 function createSquare() {
     const newSquare = document.createElement('div');
-    const dimension = '20px';
+    // const dimension = '20px';
 
     newSquare.classList.add('eas-square');
-    newSquare.style.width = dimension;
-    newSquare.style.height = dimension;
-    newSquare.style.border = 'solid purple 1px';
+    // newSquare.style.width = dimension;
+    // newSquare.style.height = dimension;
+    // newSquare.style.border = 'solid purple 1px';
 
     return newSquare;
 }
 
-// const newSquare = createSquare();
-// const otherSquare = createSquare();
-
-// gridContainer.appendChild(newSquare);
-// gridContainer.appendChild(otherSquare);
-
-function buildRow(rowNumber = 1) {
-    let columns = 10;
+function buildRow(columns = 10, rowNumber = 1) {
+    // let columns = 10;
     const newRow = document.createElement('div');
     
     newRow.classList.add('eas-row');
-    newRow.style.display = 'flex';
+    // newRow.style.display = 'flex';
 
     for (let i = 1; i <= columns; i++) {
         const square = createSquare();
@@ -36,19 +31,16 @@ function buildRow(rowNumber = 1) {
     return newRow;
 }
 
-// const newRow = buildRow();
-// gridContainer.appendChild(newRow);
-
-function buildGrid() {
-    let rows = 10;
+function buildGrid(columns = 10, rows = columns) {
+    // let rows = 10;
     const newGrid = document.createElement('div');
 
     newGrid.classList.add('eas-grid');
-    newGrid.style.display = 'flex';
-    newGrid.style.flexDirection = 'column';
+    // newGrid.style.display = 'flex';
+    // newGrid.style.flexDirection = 'column';
 
     for (let i = 1; i <= rows; i++) {
-        const row = buildRow(i);
+        const row = buildRow(columns, i);
 
         newGrid.appendChild(row);        
     }
@@ -56,5 +48,40 @@ function buildGrid() {
     return newGrid;
 }
 
-const newGrid = buildGrid();
+const newGrid = buildGrid(gridSize);
 gridContainer.appendChild(newGrid);
+
+// ----------------
+
+paintGrid = function(color = '#0000FF') {
+    let grid = document.querySelector('.eas-grid');
+    grid.addEventListener('mouseover', function(event) {
+        // squareId = event.target.id;
+        targetElement = event.target;
+        // console.log(targetElement.classList[0])
+
+        if (targetElement.classList[0] === 'eas-square'){
+            targetElement.style.backgroundColor = color;
+        }
+        // console.log(squareId);
+    })
+}
+
+paintGrid(brushColor);
+
+resizeGrid = function() {
+    let btn = document.querySelector('.eas-button');
+    btn.addEventListener('click', function() {
+        newSize = prompt('What is the new size of the grid?');
+        while (newSize > 100 || newSize < 1 || isNaN(newSize)) {
+            newSize = prompt('Your input has to be a number from 1 to 100');
+        }
+        // console.log(newSize);
+
+        gridContainer.innerHTML = "";
+        gridContainer.appendChild(buildGrid(newSize))
+        paintGrid(brushColor);
+    })
+}
+
+resizeGrid();
